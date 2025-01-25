fef7f8db08eb2557dd19c93e09d462e1633f19b2
annas2727 <annasicoli@aol.com>
2025-01-25 14:21:52 -0500

    Auth0 WIP

--- frontend/FirePower/src/LoginPage.jsx
+++ frontend/FirePower/src/LoginPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import LoginButton from './components/LoginButton'; 
+import LogoutButton from './components/LogoutButton';
+
 function LoginPage() {
-    return (
-        <div>LoginPage</div>
+    return (           
+        <main className = "column"> 
+            <h1>Auth0 Login</h1>
+            <LoginButton />
+            <LogoutButton />
+        
+        </main>
+        
     )
 }
 
