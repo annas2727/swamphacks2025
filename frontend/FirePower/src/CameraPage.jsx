f943a401b02f5dcdd89e8d249ef0fc2903747461
Luna <26190850+LunaVoid@users.noreply.github.com>
2025-01-25 11:45:47 -0500

    other

--- frontend/FirePower/src/CameraPage.jsx
+++ frontend/FirePower/src/CameraPage.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Navbar from './components/navBar'
+import './CameraPage.css'
 function CameraPage() {
+
+    const constraints = {
+        audio: true,
+        video: true
+    };
+
+    async function getMedia(constraints) {
+        console.log("test")
+        let stream = null;
+        try {
+            stream = await navigator.mediaDevices.getUserMedia(constraints);
+            console.log(stream)
+          /* use the stream */
+            
+        } catch (err) {
+          /* handle the error */
+            console.log("We are cooked chat");
+            console.log(err);
+        }
+    }
     return (
         <>
             <Navbar/>
             <div>
-                <p>Test</p>
+                <p>Camera View</p>
+                <button onClick={getMedia(constraints)} color="#841584">Click Me</button>
             </div>
             
         </>
