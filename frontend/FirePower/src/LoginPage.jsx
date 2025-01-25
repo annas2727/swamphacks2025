baa8981933d78f6f049c1dacad115f4f3b715a17
annas2727 <annasicoli@aol.com>
2025-01-25 18:13:34 -0500

    firelogo

--- frontend/FirePower/src/LoginPage.jsx
+++ frontend/FirePower/src/LoginPage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import LoginButton from './components/LoginButton'; 
 import LogoutButton from './components/LogoutButton';
+import firelogo from './assets/firelogo.jpg';
 
 function LoginPage() {
     console.log("LoginPage")
     return (           
         <main className = "column"> 
+            <img src={firelogo} className = "logo"alt="Fire Logo" />
             <h1>FlameFinder</h1>
             <p>With the California wildfires in mind, we have created a tool to help you detect fire in real time to protect peopel and their homes.</p>
             <LoginButton />
