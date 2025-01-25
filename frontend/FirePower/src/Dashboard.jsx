a44727c649cd590d8461d4757efcd06299a06e39
dang1 <dustin1204@gmail.com>
2025-01-25 11:46:41 -0500

    Added DashBoard skeleton

--- frontend/FirePower/src/Dashboard.jsx
+++ frontend/FirePower/src/Dashboard.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Dashboard() {
     return (
-        <div>Dashboard</div>
+    <>
+<div className="grid grid-cols-6 grid-rows-7 gap-4 min-h-screen">
+    <div className="col-span-4 col-start-2 text-center border-2 rounded-sm">DashBoard</div>
+    <div className="col-span-2 row-span-2 col-start-2 row-start-2 text-center border-2 rounded-sm">Last Check</div>
+    <div className="col-span-2 row-span-2 col-start-4 row-start-2 text-center border-2 rounded-sm">Fire Probability</div>
+    <div className="col-span-4 row-span-2 col-start-2 row-start-4 text-center border-2 rounded-sm">Weather</div>
+    <div className="col-span-4 row-span-2 col-start-2 row-start-6 text-center border-2 rounded-sm">Contact Authorities</div>
+</div>
+     </>
     )
 }
 
